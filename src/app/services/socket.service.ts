f1ccbd8cb5eb4c0ed8579e591245674fc534012c
NazarLutsyk <nlutsik1@gmail.com>
2018-12-05 21:06:28 +0200

    add socket client

---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {ConfigService} from './config.service';
+import * as socketIo from 'socket.io-client';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  private socket;
+
+  constructor(
+    private configService: ConfigService
+  ) {
+    this.socket = socketIo(this.configService.host);
+  }
+
+  public onEvent(event): Observable<any> {
+    return new Observable(observer => {
+      this.socket.on(event, (data) => observer.next(data));
+    });
+  }
+}
