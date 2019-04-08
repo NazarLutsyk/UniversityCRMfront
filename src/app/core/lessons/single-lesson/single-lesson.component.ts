9238d5c53cb1e9173821143def681f41ab5046de
igajduk <goloso4ok@gmail.com>
2019-04-08 12:28:56 +0300

    fix sort students in lesson

--- src/app/core/lessons/single-lesson/single-lesson.component.ts
+++ src/app/core/lessons/single-lesson/single-lesson.component.ts
@@ -77,6 +77,7 @@ export class SingleLessonComponent implements OnInit {
   }
 
   isPresent(id: number) {
+
     return this.lesson.applications.some(value => value.id == id);
   }
 
@@ -86,20 +87,13 @@ export class SingleLessonComponent implements OnInit {
   }
 
   sortArrByName(arr) {
-    const result = arr.sort(function(a, b) {
-      let nameA = a.client.name.toUpperCase(); // ignore upper and lowercase
-      let nameB = b.client.name.toUpperCase(); // ignore upper and lowercase
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
 
-      // names must be equal
-      return 0;
+    return arr.sort(function (a, b) {
+      const nameA = a.client.name.toUpperCase();
+      const nameB = b.client.name.toUpperCase();
+
+      return nameA.localeCompare(nameB);
     });
-    return result;
   }
 
 }
